f6dae0726393c4e74585fc3ebb7a13c9175acca4
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 18:58:42 -0600

    Add lazy loading for modules components

--- src/app/reactive/reactive-routing.module.ts
+++ src/app/reactive/reactive-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BasicsComponent } from './components/basics/basics.component';
+import { DynamicsComponent } from './components/dynamics/dynamics.component';
+import { SwitchesComponent } from './components/switches/switches.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'basics', component: BasicsComponent },
+      { path: 'dynamics', component: DynamicsComponent },
+      { path: 'switches', component: SwitchesComponent },
+      { path: '**', redirectTo: 'basics' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ReactiveRoutingModule { }
+export class ReactiveRoutingModule {}
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TemplateRoutingModule } from './template-routing.module';
+import { BasicsComponent } from './components/basics/basics.component';
+import { DynamicsComponent } from './components/dynamics/dynamics.component';
+import { SwitchesComponent } from './components/switches/switches.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicsComponent,
+    DynamicsComponent,
+    SwitchesComponent
+  ],
   imports: [
     CommonModule,
     TemplateRoutingModule
