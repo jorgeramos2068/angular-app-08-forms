10d107393abf8d91b05d25f533d4ad2bf7349deb
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 18:58:46 -0600

    Add customMin directive

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -6,9 +6,15 @@ import { TemplateRoutingModule } from './template-routing.module';
 import { BasicsComponent } from './components/basics/basics.component';
 import { DynamicsComponent } from './components/dynamics/dynamics.component';
 import { SwitchesComponent } from './components/switches/switches.component';
+import { CustomMinDirective } from './directives/custom-min.directive';
 
 @NgModule({
-  declarations: [BasicsComponent, DynamicsComponent, SwitchesComponent],
+  declarations: [
+    BasicsComponent,
+    DynamicsComponent,
+    SwitchesComponent,
+    CustomMinDirective,
+  ],
   imports: [CommonModule, FormsModule, TemplateRoutingModule],
 })
 export class TemplateModule {}
