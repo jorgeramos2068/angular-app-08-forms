de1b9f61c286a80ac0822ecff2518ce38b19b865
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 18:58:54 -0600

    Add validations for basic reactive form example

--- src/app/reactive/components/basics/basics.component.ts
+++ src/app/reactive/components/basics/basics.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
   templateUrl: './basics.component.html',
 })
-export class BasicsComponent {
+export class BasicsComponent implements OnInit {
   public basicsForm: FormGroup = this.formBuilder.group({
-    product: ['My value'],
-    price: [10],
-    existence: [0],
+    product: ['', [Validators.required, Validators.minLength(3)]],
+    price: [null, [Validators.required, Validators.min(0)]],
+    existence: [null, [Validators.required, Validators.min(0)]],
   });
 
   constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.basicsForm.setValue({
+      product: '',
+      price: 10,
+      existence: 10,
+    });
+  }
+
+  isValidField(field: string): boolean {
+    return <boolean>(
+      (this.basicsForm.controls[field].errors &&
+        this.basicsForm.controls[field].touched)
+    );
+  }
+
+  submit(): void {
+    if (this.basicsForm.invalid) {
+      this.basicsForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.basicsForm.value);
+    this.basicsForm.reset();
+  }
 }
