1971ed87ad4cf3ca5ec4d2390bcaaead5873d4d9
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 18:58:42 -0600

    Include menu items in sidemenu component

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MenuItem } from '../interfaces/menu-item.interface';
 
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styles: [
-  ]
 })
-export class SidemenuComponent implements OnInit {
+export class SidemenuComponent {
+  public templateMenuItems: MenuItem[] = [
+    { text: 'Basics', path: '/template/basics' },
+    { text: 'Dynamics', path: '/template/dynamics' },
+    { text: 'Switches', path: '/template/switches' },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  public reactiveMenuItems: MenuItem[] = [
+    { text: 'Basics', path: '/template/basics' },
+    { text: 'Dynamics', path: '/template/dynamics' },
+    { text: 'Switches', path: '/template/switches' },
+  ];
 }
