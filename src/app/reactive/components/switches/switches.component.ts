59c722904edbd445e1749c3c4a875131cba7ef84
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 18:59:16 -0600

    Add example for switched in Reactive forms

--- src/app/reactive/components/switches/switches.component.ts
+++ src/app/reactive/components/switches/switches.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
 })
 export class SwitchesComponent implements OnInit {
+  public switchesForm: FormGroup = this.formBuilder.group({
+    gender: ['M', Validators.required],
+    showNotifications: [true, Validators.required],
+    terms: [false, Validators.requiredTrue],
+  });
 
-  constructor() { }
+  public person = {
+    gender: 'F',
+    showNotifications: false,
+  };
+
+  public terms: boolean = false;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.switchesForm.reset(this.person);
+
+    this.switchesForm.valueChanges.subscribe({
+      next: (form) => console.log(form),
+    });
   }
 
+  submit(): void {
+    const currentValue = { ...this.switchesForm.value };
+    delete currentValue.terms;
+    this.person = currentValue;
+  }
 }
