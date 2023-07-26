e2e58418430af47918850ec8f8ff2040a716c95a
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 18:58:52 -0600

    Add template driven examples for checkboxes, radios and switches

--- src/app/template/components/switches/switches.component.ts
+++ src/app/template/components/switches/switches.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
+  public person = {
+    gender: 'F',
+    showNotifications: false,
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  public terms: boolean = false;
 }
