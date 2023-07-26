16ba1a820992884cee9f7051d7cea0b720e48791
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 18:59:22 -0600

    Create reactives form configuration for countries

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
 })
 export class SelectorPageComponent implements OnInit {
-  constructor() {}
+  public countriesForm: FormGroup = this.formBuilder.group({
+    region: ['', [Validators.required]],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  submit(): void {
+    console.log(this.countriesForm.value);
+  }
 }
